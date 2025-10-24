ad71af1534fc7c1ab514de480e736cbde3d3cf19
dean <123314446+forerunnernerd@users.noreply.github.com>
2025-10-24 21:54:01 +0200

    Removed tree and replaced with player + script

---
+++ snag-it-game/Eyeball.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@onready var anim = $AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func process(delta) -> void:
+	if Global.fullhealth == true:
+		anim.play("eye/eye_close_0")
+	if Global.threequathealth == true:
+		anim.play("eye/eye_open-25")
+	if Global.halfhealth == true:
+		anim.play("eye/eye_open_50")
+	if Global.quathealth == true:
+		anim.play("eye/eye_open_75")
+	if Global.nohealth == true:
+		anim.play("eye/eye_caught")
+	
--- snag-it-game/Global.gd
+++ snag-it-game/Global.gd
@@ -1,7 +1,6 @@
 extends Node
 
 var health := 60
-var health: int = 60
 var fullhealth: bool = true
 var threequathealth: bool = false
 var halfhealth: bool = false

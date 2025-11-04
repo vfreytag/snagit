60b25b7aa14af73da5d4238ce71c7f31e7467714
freytag-s <163901228+vfreytag@users.noreply.github.com>
2025-11-04 23:22:33 +0100

    import new animation model

--- snag-it-game/scenes/character_body_3d.gd
+++ snag-it-game/scenes/character_body_3d.gd
@@ -1,21 +1,19 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY 
+		$BUNY/AnimationPlayer.play("Armature|Jump")
 		print("i'm jumoing")
 		
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
@@ -23,11 +21,13 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		$BUNY/AnimationPlayer.play("Armature|Walk")
+		if is_on_floor():
+			$BUNY/AnimationPlayer.play("Armature|Walk")
 		#$".".look_at(direction) #can't get it to work right... so whatever im stopping for today
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		$BUNY/AnimationPlayer.play("Armature|Grab") #for some reason this one i fucked up the naming of... struggling to  actually edit the blender file now??? something broke i feel
+		if is_on_floor():
+			$BUNY/AnimationPlayer.play("Armature|Idle") #for some reason this one i fucked up the naming of... struggling to  actually edit the blender file now??? something broke i feel
 
 	move_and_slide()

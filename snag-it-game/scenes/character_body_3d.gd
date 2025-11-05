05da475f644b1316849d1e13274e71772c880780
freytag-s <163901228+vfreytag@users.noreply.github.com>
2025-11-05 20:22:42 +0100

    MAKE THAT BITCH SPIN!!!! (also commented out jump cos i dont think we need her)

--- snag-it-game/scenes/character_body_3d.gd
+++ snag-it-game/scenes/character_body_3d.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var _last_movement_direction := Vector3.BACK
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -9,10 +10,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY 
-		$BUNY/AnimationPlayer.play("Armature|Jump")
-		print("i'm jumoing")
+	#if Input.is_action_just_pressed("Jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY 
+		#$BUNY/AnimationPlayer.play("Armature|Jump")
+		#print("i'm jumoing")
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,6 +24,9 @@ func _physics_process(delta: float) -> void:
 		velocity.z = direction.z * SPEED
 		if is_on_floor():
 			$BUNY/AnimationPlayer.play("Armature|Walk")
+			_last_movement_direction = direction
+		var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+		$BUNY.global_rotation.y = lerp_angle($BUNY.global_rotation.y, target_angle, 15 * delta)
 		#$".".look_at(direction) #can't get it to work right... so whatever im stopping for today
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)

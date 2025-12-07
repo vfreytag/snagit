9353163309cff3e0a9b10113a13f603910325e16
freytag-s <163901228+vfreytag@users.noreply.github.com>
2025-12-07 15:52:15 +0100

    girl

--- snag-it-game/scripts/death.gd
+++ snag-it-game/scripts/death.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.highscore = 0
+#	Global.highscore = 0
 	pass # Replace with function body.
 
 

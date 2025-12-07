db324fc758ef0b8019bb83ce51263e42881e25ad
freytag-s <163901228+vfreytag@users.noreply.github.com>
2025-12-07 13:57:01 +0100

    edit snag mechanics

--- snag-it-game/milk.gd
+++ snag-it-game/milk.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @onready var interactable: Area3D = $RigidBody3D/CollisionShape3D/MeshInstance3D/Interactable
-#@onready var sprite_2d: Sprite2D = $Sprite2D
+
 
 func _ready() -> void:
 	interactable.interact = _on_interact
@@ -13,3 +13,7 @@ func _on_interact():
 	if interactable.is_interactable == true:
 		interactable.is_interactable = false
 		print("the player gained milk")
+		Global.word_key = $RigidBody3D/CollisionShape3D/MeshInstance3D/Interactable.item_name
+		Global.input_key = $RigidBody3D/CollisionShape3D/MeshInstance3D/Interactable.item_regex
+		print(Global.word_key)
+		print(Global.input_key)

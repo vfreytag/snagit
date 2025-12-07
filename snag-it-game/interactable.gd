eddbb4e2e21fcb9065eda4a6f2289b74a1233c5c
sarah freytag <163901228+vfreytag@users.noreply.github.com>
2025-12-07 11:18:25 +0100

    edit interactable

--- snag-it-game/interactable.gd
+++ snag-it-game/interactable.gd
@@ -2,6 +2,8 @@ extends Area3D
 
 @export var interact_name: String = ""
 @export var is_interactable: bool = true
+@export var item_name: String = ""
+@export var item_regex: String = ""
 
 var interact: Callable = func():
 	pass
